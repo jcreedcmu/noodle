346b7aaa415ec2cbfb4e5d57493683352a5e5669
Jason Reed <jcreed@gmail.com>
2017-08-03 07:30:15 -0400

    can render animation correctly now

--- anim.js
+++ anim.js
@@ -1,18 +1,22 @@
 const fs = require('fs');
 const Canvas = require('canvas');
+const sprintf = require('sprintf');
 const noodle = require('./noodle');
 
-const c = new Canvas(640, 480);
-const off = new Canvas(640, 480);
-const dd = c.getContext('2d');
-const offd = off.getContext('2d');
 
-function renderSmoothAt(t) {
-  noodle.render(dd, offd, off, noodle.REC_LEVELS, noodle.rho, 640, 480, t);
+for (i = 0; i < 60; i++) {
+  const c = new Canvas(640, 480);
+  const off = new Canvas(640, 480);
+  const dd = c.getContext('2d');
+  const offd = off.getContext('2d');
+  function renderSmoothAt(t) {
+	 noodle.render(dd, offd, off, noodle.REC_LEVELS, noodle.rho, 640, 480, t);
+  }
+  renderSmoothAt(i/60);
+  const out = fs.createWriteStream(sprintf(__dirname + '/frames/%03d.png', i));
+  c.pngStream().on('data', chunk => out.write(chunk));
 }
 
-renderSmoothAt(0.5);
-
-
-const out = fs.createWriteStream(__dirname + '/text.png');
-c.pngStream().on('data', chunk => out.write(chunk));
+// after running this script, can do
+// $ convert -delay 1.66 -loop 0 frames/*.png anim.gif
+// to generate frames
